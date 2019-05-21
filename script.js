026d28133caeb7cdc8ec4008925c8e84f659d595
tmncollins <35235207+tmncollins@users.noreply.github.com>
2019-05-21 21:07:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function newPage(name) {
+function newPage(name, num) {
   window.location.href = "quiz.html";
+  sessionStorage.setItem('quiz_num', num)
+  sessionStorage.setItem('quiz_name', name)
+}
+
+function getQuizName() {
+  return sessionStorage.getItem('quiz_name')
 }
