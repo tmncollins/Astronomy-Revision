5db6ad80f3ebfb7c0ef0476fbea476188410bc26
tmncollins <35235207+tmncollins@users.noreply.github.com>
2019-05-22 07:17:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,9 +23,31 @@ function readTextFile(file)
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText = rawFile.responseText;
-                alert(allText);
+                return allText;
             }
         }
     }
     rawFile.send(null);
+}
+
+function makeQuiz()  {
+  var f = document.createElement("form");
+f.setAttribute('method',"post");
+f.setAttribute('action',"submit.php");
+
+var i = document.createElement("input"); //input element, text
+i.setAttribute('type',"text");
+i.setAttribute('name',"username");
+
+var s = document.createElement("input"); //input element, Submit button
+s.setAttribute('type',"submit");
+s.setAttribute('value',"Submit");
+
+f.appendChild(i);
+f.appendChild(s);
+
+//and some more input elements here
+//and dont forget to add a submit button
+
+document.getElementsByTagName('body')[0].appendChild(f);  
 }
