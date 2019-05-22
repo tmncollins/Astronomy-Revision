a9236b9bf7e4ee45f10bcf19bc4fb462fabb6a28
tmncollins <35235207+tmncollins@users.noreply.github.com>
2019-05-22 17:29:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ function readTextFile(file)
 }
 
 function makeQuiz()  {
+  var d = document.createElement("div");
   var f = document.createElement("form");
 f.setAttribute('method',"post");
 f.setAttribute('action',"submit.php");
 
 var i = document.createElement("input"); //input element, text
-i.setAttribute('type',"text");
-i.setAttribute('name',"username");
+i.setAttribute('type',"number");
 
 var s = document.createElement("input"); //input element, Submit button
 s.setAttribute('type',"submit");
@@ -45,9 +45,9 @@ s.setAttribute('value',"Submit");
 
 f.appendChild(i);
 f.appendChild(s);
-
+  
 //and some more input elements here
 //and dont forget to add a submit button
-
-document.getElementsByTagName('body')[0].appendChild(f);  
+d.appendChild(f)
+document.getElementsByTagName('body')[0].appendChild(d);  
 }
