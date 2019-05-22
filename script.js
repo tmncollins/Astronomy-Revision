49dee3d6ea50bbb2d0501dd31a2be23782c0f72e
tmncollins <35235207+tmncollins@users.noreply.github.com>
2019-05-22 07:11:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ function getQuizName() {
 function returnString() {
   return "Hi!"; 
 }
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
