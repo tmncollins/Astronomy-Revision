128d755248b65dc275044a7cca22274ca457a040
tmncollins <35235207+tmncollins@users.noreply.github.com>
2019-05-24 21:25:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -139,18 +139,7 @@ lines.forEach(function(item, index) {
 });
   
   
-  
-  var i = document.createElement("input"); //input element, text
-  i.setAttribute('type',"number");
- 
-  var s = document.createElement("input"); //input element, Submit button
-  s.setAttribute('type',"submit");
-  s.setAttribute('value',"Submit");
- 
-  f.appendChild(i);
-  f.appendChild(s);
-  
-//and some more input elements here
+  //and some more input elements here
 //and dont forget to add a submit button
 d.appendChild(f);
 document.getElementsByTagName('body')[0].appendChild(d);  
