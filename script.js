b9745581d7c403b41946bf649ab10f0bdbcfcac5
tmncollins <35235207+tmncollins@users.noreply.github.com>
2019-05-31 14:09:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function getQuizNum() {
   return sessionStorage.getItem('quiz_num');
 }
 function getQuizFileName(num) {
-  var array = [ "naked-eye-quiz.txt", "", "earth-quiz.txt", "moon-quiz.txt", "sun-quiz.txt", "", "stars-quiz.txt", "comets-quiz.txt", "exoplanet-quiz.txt", "", "stars-quiz.txt", "star-evolution.txt", "galaxy-quiz.txt", "cosmology-quiz.txt", "telescope-quiz.txt", "exploration-quiz.txt"];
+  var array = [ "naked-eye-quiz.txt", "", "earth-quiz.txt", "moon-quiz.txt", "sun-quiz.txt", "solar-system-1.txt", "", "comets-quiz.txt", "exoplanet-quiz.txt", "", "stars-quiz.txt", "star-evolution.txt", "galaxy-quiz.txt", "cosmology-quiz.txt", "telescope-quiz.txt", "exploration-quiz.txt"];
   return array[num-1];
 }
 
