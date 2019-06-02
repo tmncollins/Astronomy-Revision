9218b7cf8d95db8998504c43240c0d021e061f39
tmncollins <35235207+tmncollins@users.noreply.github.com>
2019-06-02 08:28:19 +0100

    Update mark.js

--- mark.js
+++ mark.js
@@ -109,7 +109,7 @@ alert(text);
 			if (type == " ww " || type == " wn ") {
 				var p = document.getElementById("ans" + i.toString(10));
 				p.setAttribute('class', "show");
-			} else if (type == "rb") {
+			} else if (type == " rb ") {
 				var a = document.getElementById(i.toString(0) + "rb" + ans.trim());
 				a.setAttribute('class', "green");
 			}
